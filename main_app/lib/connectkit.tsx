b59d016150ced385390d0419df603f7fb297f3d4
anurag <anurags9693@gmail.com>
2026-01-15 12:27:39 +0530

    feat: Introduce admin order and role management APIs, and implement gasless USDT transfers using Particle Network.

--- main_app/lib/connectkit.tsx
+++ main_app/lib/connectkit.tsx
@@ -80,6 +80,7 @@ const config = createConfig({
       '--pcm-overlay-backdrop-filter': 'blur(8px)',
       '--pcm-modal-box-shadow': '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
     },
+    logo: '/srd.jpg',
     recommendedWallets: [   
       { walletId: "bianance", label: "Popular" },
     ],
