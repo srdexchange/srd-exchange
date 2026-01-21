c8fbe12adee1d427cf8ee0d4bf46031a0de727d2
anurag <anurags9693@gmail.com>
2026-01-21 10:44:33 +0530

    sidebar_context

---
+++ main_app/context/SidebarContext.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+
+interface SidebarContextType {
+    isSidebarOpen: boolean;
+    setIsSidebarOpen: (isOpen: boolean) => void;
+    openSidebar: () => void;
+    closeSidebar: () => void;
+}
+
+const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
+
+export const SidebarProvider = ({ children }: { children: ReactNode }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const openSidebar = () => setIsSidebarOpen(true);
+    const closeSidebar = () => setIsSidebarOpen(false);
+
+    return (
+        <SidebarContext.Provider value={{ isSidebarOpen, setIsSidebarOpen, openSidebar, closeSidebar }}>
+            {children}
+        </SidebarContext.Provider>
+    );
+};
+
+export const useSidebar = () => {
+    const context = useContext(SidebarContext);
+    if (context === undefined) {
+        throw new Error('useSidebar must be used within a SidebarProvider');
+    }
+    return context;
+};
